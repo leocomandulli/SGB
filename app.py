ce9d02450258ca2ae9f5fc709ba113c57f2aa16d
Leonardo Comandulli <139185818+leocomandulli@users.noreply.github.com>
2024-11-06 10:36:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import sqlite3
 import streamlit as st
 import pandas as pd
-import streamlit_authenticator as stauth
 from streamlit_option_menu import option_menu
 
 # ----- Importação das páginas ------
